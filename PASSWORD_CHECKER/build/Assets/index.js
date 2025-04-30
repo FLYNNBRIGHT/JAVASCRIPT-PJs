de5f317e9ae37362ac6437263a5db23fc8912a41
FLYNNBRIGHT <flynnbright@gmail.com>
2025-04-30 17:16:50 +0100

    Added the Password Checker Function

--- PASSWORD_CHECKER/build/Assets/index.js
+++ PASSWORD_CHECKER/build/Assets/index.js
@@ -1,13 +1,17 @@
 const user_input = document.querySelector("#user_password");
 const eye_icon = document.querySelector("#eye");
-const user_password = user_input.value;
-const outcome_active = document.querySelector("#outcome_active");
+const check_password = document.querySelector("#check");
 
-const lowercase_active = document.querySelector("#lower");
-const uppercase_active = document.querySelector("#upper");
-const numbers_active = document.querySelector("#number");
-const length_active = document.querySelector("#length");
-const special_active = document.querySelector("#special");
+// const lowercase_active = document.querySelector("#lower");
+// const uppercase_active = document.querySelector("#upper");
+// const numbers_active = document.querySelector("#number");
+// const length_active = document.querySelector("#length");
+// const special_active = document.querySelector("#special");
+
+// const no_pass = document.querySelector("#no_pass");
+// const weak_pass = document.querySelector("#weak_pass");
+// const med_pass = document.querySelector("#med_pass");
+// const str_pass = document.querySelector("#str_pass");
 
 eye_icon.addEventListener("click", () => {
     eye_icon.classList.toggle("fa-eye");
@@ -16,19 +20,22 @@ eye_icon.addEventListener("click", () => {
     user_input.type = user_input.type === "password" ? "text" : "password";
 });
 
+check_password.addEventListener("click", () => {
+    // console.log(user_input_password);
+    const user_input_password = user_input.value;
 
-user_input.addEventListener("keypress", () => {
-    outcome_active.classList.remove("hidden");
 
-    if (user_password.length > 8) {
-        length_active.classList.remove("not_match");
-        length_active.classList.add("match");
-        console.log("length = 8");
-        
-    }
+    const uppercase_letters = /[A-Z]/.test(user_input_password);
+    const lowercase_letters = /[a-z]/.test(user_input_password);
+    const spec_char = /[\W_]/.test(user_input_password);
+    const numbers = /[\d]/.test(user_input_password);
+    const password_length = user_input_password.length;
 
-    else {
-        length_active.classList.remove("match");
+    if (password_length >= 8 && uppercase_letters && spec_char && lowercase_letters && numbers) {
+        console.log("Strong Password");
+    } else if (password_length >= 8 && spec_char && numbers) {
+        console.log("Medium Password");
+    } else {
+        console.log("Weak Password");
     }
 });
-
